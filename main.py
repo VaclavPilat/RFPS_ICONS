d0ac5ac3903f6ebdb6a2cba882c7e0ef6b5f209a
Václav Pilát <pilatvac@fit.cvut.cz>
2024-07-20 23:36:26 +0200

    Multiplayer icon

--- main.py
+++ main.py
@@ -21,3 +21,14 @@ def Singleplayer(draw: ImageDraw, size: int, outline: tuple, fill: tuple, width:
     draw.ellipse([0.25*size, 0, 0.75*size, size/2], fill, outline, width)
     draw.arc([0, size/2-width, size, 1.5*size], -180, 180, fill, width)
     draw.line([0, size-width/2, size, size-width/2], fill, width)
+
+@icon()
+def Multiplayer(draw: ImageDraw, size: int, outline: tuple, fill: tuple, width: float) -> None:
+    def person(draw: ImageDraw, size: int, outline: tuple, fill: tuple, width: float, x: float, y: float):
+        draw.ellipse([0.25*size+x-width, y-width, 0.75*size+x+width, size/2+y+width], (0,0,0,0), None, width*2)
+        draw.arc([x-width, size/2-width+y-width, size+x+width, 1.5*size+y+width], -180, 0, (0,0,0,0), width*2)
+        draw.ellipse([0.25*size+x, y, 0.75*size+x, size/2+y], fill, outline, width)
+        draw.arc([x, size/2-width+y, size+x, 1.5*size+y], -180, 0, outline, width)
+        draw.line([x, size-width/2+y, size+x, size-width/2+y], outline, width)
+    person(draw, size*2/3, outline, fill, width, size/3, 0)
+    person(draw, size*2/3, outline, fill, width, 0, size/3)
