e2288cdb00640107dbb4caede9713e7d3f2e8395
Václav Pilát <pilatvac@fit.cvut.cz>
2024-07-20 23:03:27 +0200

    Singleplayer icon

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def icon() -> None:
             size = size*sample,
             outline = (255, 255, 255),
             fill = None,
-            width = 5*sample,
+            width = 10*sample,
         )
         image = image.resize((size, size), resample=Image.LANCZOS)
         image.save("{}.png".format(func.__name__), "PNG")
@@ -18,4 +18,6 @@ def icon() -> None:
 
 @icon()
 def Singleplayer(draw: ImageDraw, size: int, outline: tuple, fill: tuple, width: float) -> None:
-    draw.ellipse((0, 0, size, size), fill, outline, width)
+    draw.ellipse([0.25*size, 0, 0.75*size, size/2], fill, outline, width)
+    draw.arc([0, size/2-width, size, 1.5*size], -180, 180, fill, width)
+    draw.line([0, size-width/2, size, size-width/2], fill, width)
