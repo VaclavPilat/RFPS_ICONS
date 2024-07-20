b3d05f64c6207d59fe239a5160d31b1bfac5141f
Václav Pilát <pilatvac@fit.cvut.cz>
2024-07-20 20:51:40 +0200

    Added resizing

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from PIL import Image, ImageDraw
 
 def icon() -> None:
     def wrap(func) -> None:
-        image = Image.new("RGBA", (100, 100), (255, 255, 255, 0))
-        draw = ImageDraw.Draw(image)
-        func(draw)
+        size = 100
+        sample = 5
+        image = Image.new("RGBA", (size*sample, size*sample), (255, 255, 255, 0))
+        func(
+            draw = ImageDraw.Draw(image),
+            size = size*sample,
+            outline = (255, 255, 255),
+            fill = None,
+            width = 5*sample,
+        )
+        image = image.resize((size, size), resample=Image.LANCZOS)
         image.save("{}.png".format(func.__name__), "PNG")
     return wrap
 
 @icon()
-def Singleplayer(draw: ImageDraw) -> None:
-    pass
+def Singleplayer(draw: ImageDraw, size: int, outline: tuple, fill: tuple, width: float) -> None:
+    draw.ellipse((0, 0, size, size), fill, outline, width)
