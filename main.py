22344a69084c01fb4d7635f2bdc7326f066944d6
Václav Pilát <pilatvac@fit.cvut.cz>
2024-09-01 18:45:38 +0200

    Metro icon

--- main.py
+++ main.py
@@ -184,10 +184,10 @@ def Trash(self):
 @CreateIcon
 def Metro(self):
     for i in range(2):
-        self.line([(self.size/2, self.size*0.6), (i*self.size, self.size+self.width)])
+        self.line([(self.size/2, self.size*0.5), (i*self.size, self.size+self.width)])
     x = 0.2
-    y = 0.7
-    self.rectangle([(0, 0), (self.size, y*self.size+self.width*1.5)], fill=self.background, outline=self.background)
+    y = 0.75
+    self.rectangle([(0, 0), (self.size, y*self.size+self.width)], fill=self.background, outline=self.background)
     self.line([(x*self.size, self.width/2), ((1-x)*self.size, self.width/2), ((1-x)*self.size, y*self.size), (x*self.size, y*self.size)], True, polygon=True)
     self.rectangle([(x*self.size, y*self.size-self.width*2.5), ((1-x)*self.size, y*self.size)])
     self.ellipse([(x*self.size+self.width*0.5, y*self.size-self.width*1.5), (x*self.size+self.width*1.5, y*self.size-self.width*0.5)], outline=self.background)
