78a36a9d183fcd260fe9a077be1660c0542b4a38
Václav Pilát <pilatvac@fit.cvut.cz>
2025-01-16 19:58:55 +0100

    Added back 3 more icons and the complete icon list

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ from Image import createImage
 # \image html Users.png
 # \image html Sliders.png
 # \image html Graph.png
+# \image html Audio.png
+# \image html Monitor.png
+# \image html Splitscreen.png
+# \image html Cap.png
+# \image html Cogs.png
+# \image html Keyboard.png
+# \image html Play.png
+# \image html Warehouse.png
+# \image html Shutdown.png
+# \image html Home.png
+# \image html Search.png
+# \image html Flag.png
+# \image html Cursor.png
+# \image html Book.png
+# \image html Circle.png
+# \image html Plus.png
+# \image html Trash.png
+# \image html Metro.png
+# \image html Babel.png
+# \image html Stopwatch.png
+# \image html Crosshair.png
 
 
 
@@ -51,3 +72,23 @@ def Graph(self, W, H, L, C, B) -> None:
     self.line(*points)
     for x, y in points:
         self.ellipse((x-L*0.8, y-L*0.8), (x+L*0.8, y+L*0.8))
+
+## \image html Audio.png
+@createImage()
+def Audio(self, W, H, L, C, B) -> None:
+    self.line((L/2, H/3), (L/2, H*2/3), (W/4, H*2/3), (W/2, H-L), (W/2, L), (W/4, H/3), (L/2, H/3), rounded=True)
+    self.arc((0, 0), (W, H), start=-60, end=60)
+    self.arc((W/3, H/4), (W*6.25/8, H*3/4), start=-65, end=65)
+
+## \image html Monitor.png
+@createImage()
+def Monitor(self, W, H, L, C, B) -> None:
+    self.line((W/4, H-L/2), (W*3/4, H-L/2), rounded=True)
+    self.line((L/2, H*3.6/5), (L/2, L/2), (W-L/2, L/2), (W-L/2, H*3.2/5), (L/2, H*3.2/5), (L/2, H*3.6/5), (W-L/2, H*3.6/5), (W-L/2, H/2), rounded=True)
+    self.line((W/2-L/2, H-L/2), (W/2-L/2, H*3.6/5), (W/2+L/2, H*3.6/5), (W/2+L/2, H-L/2), rounded=True)
+
+## \image html Splitscreen.png
+@createImage()
+def Splitscreen(self, W, H, L, C, B) -> None:
+    self.load(Monitor, (W, H), (0, 0))
+    self.line((W/2, 0), (W/2, H))
