ed3592c67a8b057c19cb20a1cfdba95d6f5b2ec2
Václav Pilát <pilatvac@fit.cvut.cz>
2025-01-24 01:20:09 +0100

    Separated setting decorators from Image file

--- src/Utils/Image.py
+++ src/Utils/Image.py
@@ -1,55 +1,8 @@
 ## \file
 # Functionality for generating images using simple shapes
+from Utils.Settings import *
 from PIL import ImageDraw
 import math
-
-
-
-def defaultSettings(*names) -> "func":
-    """Decorator for passing some default settings through and allowing their updates
-
-    Returns:
-        func: Wrapper for a drawing method
-    """
-    def wrapper(method: "func") -> "func":
-        def wrapped(self, *points, **settings) -> None:
-            values = {k: v for k, v in {
-                "width": self.width,
-                "outline": self.color,
-                "fill": self.color,
-                "start": 0,
-                "end": 180,
-                "joint": "curve",
-                "radius": self.width,
-                "corners": (True, True, True, True),
-            }.items() if k in names}
-            values.update(settings)
-            method(self, *self.transform(points), **values)
-        return wrapped
-    return wrapper
-
-
-
-def defaultLoadSettings(method: "func") -> "func":
-    """Decorator for providing default settings for loaded image functions
-
-    Args:
-        method (func): Method being wrapped
-
-    Returns:
-        func: Wrapped method
-    """
-    def wrapped(self, function: "func", **settings) -> None:
-        values = {
-            "size": self.size,
-            "offset": (0, 0),
-            "color": self.color,
-            "background": self.background,
-            "width": self.width,
-        }
-        values.update(settings)
-        method(self, function, **values)
-    return wrapped
 
 
 
