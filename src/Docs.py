1d380755e3ff29d08e42dd4ca1df314cfd35b49e
Václav Pilát <pilatvac@fit.cvut.cz>
2025-01-28 12:05:30 +0100

    Shortened strafing image syntax

--- src/Docs.py
+++ src/Docs.py
@@ -42,30 +42,27 @@ def BasicDots(self, W, H, L, C, B) -> None:
     self.dot((W/2-SI*H/2+L/2, H/2))
     self.dot((W/2+SI*H/2-L/2, H/2))
 
-@createImage("Docs", (300, 300), color=(0, 0, 0))
+@createImage("Docs", (300, 300), color=(150, 150, 150))
 ## \image html Docs/Strafing.png
 def Strafing(self, W, H, L, C, B) -> None:
     self.load(Arrows, color=(200, 200, 200))
-    self.roundedRectangle((W/2-SI*H/2, (1-FO)/2*H), (W/2+SI*H/2, (H+H*BA)/2), fill=None, outline=(150, 150, 150))
-    self.load(BasicDots)
-    self.dot((W/2-SI*H/2+L/2, (1-FO)/2*H+L/2))
-    self.dot((W/2+SI*H/2-L/2, (1-FO)/2*H+L/2))
-    self.dot((W/2-SI*H/2+L/2, (H+H*BA)/2-L/2))
-    self.dot((W/2+SI*H/2-L/2, (H+H*BA)/2-L/2))
+    self.roundedRectangle((W/2-SI*H/2, (1-FO)/2*H), (W/2+SI*H/2, (H+H*BA)/2), fill=None)
+    self.load(BasicDots, color=(0, 0, 0))
+    self.dot((W/2-SI*H/2+L/2, (1-FO)/2*H+L/2), outline=(0, 0, 0))
+    self.dot((W/2+SI*H/2-L/2, (1-FO)/2*H+L/2), outline=(0, 0, 0))
+    self.dot((W/2-SI*H/2+L/2, (H+H*BA)/2-L/2), outline=(0, 0, 0))
+    self.dot((W/2+SI*H/2-L/2, (H+H*BA)/2-L/2), outline=(0, 0, 0))
 
-@createImage("Docs", (300, 300), color=(0, 0, 0))
+@createImage("Docs", (300, 300), color=(150, 150, 150))
 ## \image html Docs/NoStrafing.png
 def NoStrafing(self, W, H, L, C, B) -> None:
     self.load(Arrows, color=(200, 200, 200))
-    self.arc((W/2-FO*H/2, (1-FO)/2*H), (W/2+FO*H/2, (H+H*FO)/2), start=180, end=360, fill=(150, 150, 150), rounded=True)
-    self.arc((W/2-SI*H/2, (1-SI)/2*H), (W/2+SI*H/2, (H+H*SI)/2), start=0, end=360, fill=(150, 150, 150))
-    self.arc((W/2-BA*H/2, (1-BA)/2*H), (W/2+BA*H/2, (H+H*BA)/2), start=0, end=180, fill=(150, 150, 150), rounded=True)
-    self.arc((W/2-FS*H/2, (1-FS)/2*H), (W/2+FS*H/2, (H+H*FS)/2), start=180, end=360, fill=(150, 150, 150), rounded=True)
-    self.arc((W/2-BS*H/2, (1-BS)/2*H), (W/2+BS*H/2, (H+H*BS)/2), start=0, end=180, fill=(150, 150, 150), rounded=True)
-    self.load(BasicDots)
-    for angle in [-45, -135]:
+    self.arc((W/2-FO*H/2, (1-FO)/2*H), (W/2+FO*H/2, (H+H*FO)/2), start=180, end=360, rounded=True)
+    self.arc((W/2-SI*H/2, (1-SI)/2*H), (W/2+SI*H/2, (H+H*SI)/2), start=0, end=360)
+    self.arc((W/2-BA*H/2, (1-BA)/2*H), (W/2+BA*H/2, (H+H*BA)/2), start=0, end=180, rounded=True)
+    self.arc((W/2-FS*H/2, (1-FS)/2*H), (W/2+FS*H/2, (H+H*FS)/2), start=180, end=360, rounded=True)
+    self.arc((W/2-BS*H/2, (1-BS)/2*H), (W/2+BS*H/2, (H+H*BS)/2), start=0, end=180, rounded=True)
+    self.load(BasicDots, color=(0, 0, 0))
+    for speed, angle in ((FS, -45), (FS, -135), (BS, 45), (BS, 135)):
         sin, cos = (function(math.radians(angle)) for function in [math.sin, math.cos])
-        self.dot((W/2+cos*(FS*H/2-L/2), H/2+sin*(FS*H/2-L/2)))
-    for angle in [45, 135]:
-        sin, cos = (function(math.radians(angle)) for function in [math.sin, math.cos])
-        self.dot((W/2+cos*(BS*H/2-L/2), H/2+sin*(BS*H/2-L/2)))
+        self.dot((W/2+cos*(speed*H/2-L/2), H/2+sin*(speed*H/2-L/2)), outline=(0, 0, 0))
